c3f75853fce825dbecd2e84b1d93171db869daf3
Harendra Singh Bisht <harendra87@gmail.com>
2017-02-11 15:30:08 +0530

    Commit message

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,7 @@
+@extends ('layouts.master')
+
+	@section ('content')
+
+	<h1>This is another sample text</h1>
+
+	@endsection
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+       
+    <title></title>
+
+</head>
+
+<body>
+       
+    <ul>
+        
+            <h1>{{ $task->body }}</h1>
+
+    </ul>
+
+
+</body>
+
+</html>
