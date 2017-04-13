f5f5a118e35f46cec65561f1d31eb14d570a5401
Harendra Singh Bisht <harendra87@gmail.com>
2017-04-13 21:51:58 +0530

    Completed Video Tutorial 21 View Composers

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,34 +14,16 @@ class PostsController extends Controller
 	}
 
 
-	public function index()
+	public function index() 
 	{
-		$posts = Post::latest();
+		$posts = Post::latest()
+			->filter(request(['month', 'year']))
+			->get();
+		
+	
 
 
-		if ($month= request('month')) {
-
-			$posts->whereMonth('created_at', Carbon::parse($month)->month);
-
-		}
-
-		if ($year= request('year')) {
-
-			$posts->whereYear('created_at', $year);
-
-		}
-
-		$posts = $posts->get();
-
-
-		$archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-			->groupBy('year', 'month')
-			->orderByRaw('min(created_at) desc')
-			->get()
-			->toArray();
-
-
-		return view('posts.index', compact('posts', 'archives'));
+		return view('posts.index', compact('posts'));
 	}
 
 
